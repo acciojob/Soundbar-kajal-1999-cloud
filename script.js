0488c97d6d0c2637305ecefe443e0eff89939fdb
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-16 12:30:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// taking an empty variable to attach play/stop/pause in the global
+
+var sound = new Sound();
+  //  <audio src="./test.mp3" controls></audio>
+function playSound(fileName){
+	// stopSound();
+	sound.src = 'sounds/' + fileName ;
+	sound.play();
+}
+
+function stopSound(fileName){
+	sound.pause()
+}
