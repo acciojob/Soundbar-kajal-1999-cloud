ccf95fe83b2bc7ba8ff527a10e58b3fc3412943b
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-16 12:53:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-//your JS code here. If required.
-// taking an empty variable to attach play/stop/pause in the global
+// //your JS code here. If required.
+// // taking an empty variable to attach play/stop/pause in the global
 
-var sound = new Audio();
-  //  <audio src="./test.mp3" controls></audio>
-function playSound(){
-	// stopSound();
-	sound.src = "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3" ;
-	sound.play();
+// var sound = new Audio();
+//   //  <audio src="./test.mp3" controls></audio>
+// function playSound(){
+// 	// stopSound();
+// 	sound.src = './sounds' + ;
+// 	sound.play();
+// }
+
+// function stopSound(fileName){
+// 	sound.pause()
+// }
+
+
+
+
+// //your JS code here. If required.
+// var audio = new Audio();
+// function playSound(filename) {
+// 	setTimeout(()=>{
+// 	audio.src = 'sounds/' + filename;
+//     audio.play();
+// 	},1000);
+	
 }
-
-function stopSound(fileName){
-	sound.pause()
+function stopSound() {
+	audio.pause();
+    audio.currentTime = 0;
 }
